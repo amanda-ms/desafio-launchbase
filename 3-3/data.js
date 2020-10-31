d1759815dbc30a4f52db6c95528697e2f458d68d
Amanda Silva <amanda.silva@server.signainfo.com.br>
2020-10-31 15:57:47 -0300

    arquivos nunjuncks e dados dinâmicos

---
+++ 3-3/data.js
@@ -0,0 +1,20 @@
+module.exports = [
+  {
+    id: "react-material-ui",
+    title: "Integração do Material UI com ReactJS",
+    author: "por Higo Ribeiro",
+    hour: "5min",
+  },
+  {
+    id: "ambiente-desenvolvimento-javascript",
+    title: "Ambiente Javascript: Dicas, VSCode e terminal",
+    author: "por Diego Fernandes",
+    hour: "4min",
+  },
+  {
+    id: "context-api-react-16-ciclo-de-vida",
+    title: "Context API do React 16.3.0 e métodos de ciclo de vida",
+    author: "por Diego Fernandes",
+    hour: "4min",
+  },
+];
