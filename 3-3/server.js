6f397fe9dc7ed73e48f616f6feca79ee6856db3a
Amanda Silva <amanda-ms@hotmail.com>
2020-11-23 11:16:26 -0300

    Dados dinâmicos página sobre

--- 3-3/server.js
+++ 3-3/server.js
@@ -13,7 +13,20 @@ nunjucks.configure("views", {
 });
 
 server.get("/", function (req, res) {
-  return res.render("about");
+  const about = {
+    avatar_company:
+      "https://media-exp1.licdn.com/dms/image/C560BAQFU-ZKLLdANXg/company-logo_200_200/0?e=1609372800&v=beta&t=MVXb_ey8pT8NpMZXOJwMiY1QErQnxSCd7NEiiWPb9Rs",
+    name_company: "Rocketseat",
+    links: [
+      { name: "Github", url: "https://github.com/Rocketseat" },
+      {
+        name: "Instragam",
+        url: "https://www.instagram.com/rocketseat_oficial/",
+      },
+      { name: "Facebook", url: "https://www.facebook.com/rocketseat" },
+    ],
+  };
+  return res.render("about", { about });
 });
 
 server.get("/courses", function (req, res) {
